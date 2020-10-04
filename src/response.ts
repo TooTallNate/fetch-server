b24b1b67938fbbc52eaec52b0d2f2f823a46a8bf
Nathan Rajlich <nathan@tootallnate.net>
2020-10-03 23:24:01 -0700

    Support JSON `res.body`

--- src/response.ts
+++ src/response.ts
@@ -6,7 +6,7 @@ import { ResponseSymbol } from './types';
 export default class Response {
 	readonly headers: Headers;
 	status: number;
-	body?: string | Buffer | Readable;
+	body?: string | Buffer | Readable | any;
 	[ResponseSymbol]: http.ServerResponse;
 
 	constructor(res: http.ServerResponse) {
@@ -29,9 +29,17 @@ export default class Response {
 			res.setHeader(name, value);
 		}
 		if (Buffer.isBuffer(this.body) || typeof this.body === 'string') {
+			if (!this.headers.has('content-type')) {
+				res.setHeader('content-type', 'text/plain; charset=utf8');
+			}
 			res.end(this.body);
 		} else if (this.body instanceof Readable) {
 			this.body.pipe(res);
+		} else if (typeof this.body !== 'undefined') {
+			if (!this.headers.has('content-type')) {
+				res.setHeader('content-type', 'application/json; charset=utf8');
+			}
+			res.end(JSON.stringify(this.body));
 		} else {
 			res.end();
 		}
